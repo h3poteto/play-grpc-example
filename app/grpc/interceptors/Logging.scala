fb820620913e69e79db309e75e2d7f58031e4804
AkiraFukushima <h3.poteto@gmail.com>
2017-10-05 11:50:48 +0900

    Add response logging

--- app/grpc/interceptors/Logging.scala
+++ app/grpc/interceptors/Logging.scala
@@ -2,9 +2,12 @@ package grpc.interceptors
 
 // https://github.com/grpc/grpc-java/blob/166108a9438c22d06eb3b371b5ad34a75e14787c/examples/src/main/java/io/grpc/examples/header/HeaderServerInterceptor.java
 // http://blog.soushi.me/entry/2017/08/18/234615
+// https://github.com/grpc/grpc-java/issues/2280
+// https://github.com/grpc/grpc-java/issues/1949
+// https://github.com/saturnism/grpc-java-demos/blob/master/error-handling-example/error-server/src/main/java/com/example/grpc/server/UnknownStatusDescriptionInterceptor.java
 
 import io.grpc.{ServerInterceptor, ServerCall, ServerCallHandler, Metadata, Status}
-import io.grpc.ForwardingServerCallListener.SimpleForwardingServerCallListener
+import io.grpc.ForwardingServerCall.SimpleForwardingServerCall
 import play.Logger
 
 class Logging() extends ServerInterceptor {
@@ -13,8 +16,19 @@ class Logging() extends ServerInterceptor {
     requestHeaders: Metadata,
     next: ServerCallHandler[ReqT, RespT]): ServerCall.Listener[ReqT] = {
 
-    Logger.info(requestHeaders.toString() + ", " + call.getAttributes().toString() + ", " + call.getMethodDescriptor().getFullMethodName())
-    return next.startCall(call, requestHeaders)
+    Logger.info("Request: " + requestHeaders.toString() + ", " + call.getAttributes().toString() + ", " + call.getMethodDescriptor().getFullMethodName())
+
+    val wrapperCall: ServerCall[ReqT, RespT] = new SimpleForwardingServerCall[ReqT, RespT](call) {
+      override def sendMessage(message: RespT) {
+        Logger.info("Response: " + message.toString())
+        super.sendMessage(message)
+      }
+      override def close(status: Status, trailers: Metadata) {
+        Logger.info("Response: " + status.toString())
+        super.close(status, trailers)
+      }
+    }
+    return next.startCall(wrapperCall, requestHeaders)
   }
 }
 
